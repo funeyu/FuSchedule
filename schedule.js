a16bafd9ba8f53a352237001532146d1e1dad041
傅鹤雨 <fuheyu@meituan.com>
2018-03-08 23:37:47 +0800

    初始Job

--- schedule.js
+++ schedule.js
@@ -1,5 +1,43 @@
 class Job {
-    constructor() {
+    constructor(preJob, jobFunc, cronTime) {
+        this.jobFunc = jobFunc
+        this.cronTime = cronTime
+        this.preJob = preJob
+        this.preJob.nextJob = this
+        this.nextJob(null)
+    }
+
+    setNext(job) {
+        this.nextJob = job;
+        job.preJob = this
+        return this
+    }
+
+    getNextJob() {
+        return this.nextJob
+    }
+
+    getPreJob() {
+        return this.preJob
+    }
+
+    remove() {
+        // job队列中的head 是个空Job，一直存在不会被删除
+        let preJob = this.getPreJob()
+        let nextJob = this.getNextJob()
+
+        preJob.setNext(nextJob)
+    }
+}
+
+const JobChainHead = new Job(null, function() {})
+
+let JobChainTail
+
+const SheduleJob = function(cronTime, func) {
+    if(!JobChainTail) {
 
     }
 }
+
+export default SheduleJob
